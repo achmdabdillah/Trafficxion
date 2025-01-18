1e28d2c8ee7ed70cb747a5dca50d7945d1e1755b
achmdabdillah <achmdabdil9@gmail.com>
2025-01-18 19:12:07 +0700

    add dynamics to OpenStreetMap componenet

--- src/app/page.js
+++ src/app/page.js
@@ -1,35 +1,17 @@
 "use client";
 import Image from "next/image";
-import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import { useState, useEffect } from "react";
 import Link from "next/link";
+import dynamic from "next/dynamic";
 
 // app/IconExample.js
 
 import Header from "./Components/Header";
 import NewsCard from "./Components/NewsCard";
 import ParkCard from "./Components/ParkCard";
-
-const OpenStreetMap = ({ position }) => {
-  return (
-    <MapContainer
-      center={position}
-      zoom={13}
-      style={{ height: "400px", width: "100%" }}
-    >
-      <TileLayer
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-      />
-      <Marker position={position}>
-        <Popup>
-          A pretty CSS3 popup.
-          <br /> Easily customizable.
-        </Popup>
-      </Marker>
-    </MapContainer>
-  );
-};
+const OpenStreetMap = dynamic(() => import("./Components/OpenStreetMap"), {
+  ssr: false,
+});
 
 export default function Home() {
   const [userLocation, setUserLocation] = useState(null);
